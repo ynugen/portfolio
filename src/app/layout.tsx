c9e5e3ac87e181a1ddb71b6990045fb2b825a630
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-04-08 22:50:17 +1000

    Start on landing page and sidebar component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "AYIMA",
-  description: "Nguyen MT A. - Portfolio",
-};
+import Sidebar from "./components/Sidebar";
 
 export default function RootLayout({
   children,
@@ -24,9 +12,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="sidebar flex-row jusify-start">
+        <Sidebar />
         {children}
       </body>
     </html>
