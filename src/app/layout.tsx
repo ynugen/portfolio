247917ea63bcac246344a018a8b1cd02a824bad5
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-04-12 16:29:04 +1000

    Restore correct Sidebar.tsx casing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "./globals.css";
-import Sidebar from "./components/Sidebar_TEMP";
+import Sidebar from "./components/Sidebar";
 import DynamicCSS from "./components/DynamicCSS";
 
 export default function RootLayout({
