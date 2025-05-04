9b9534becb43b58e6fab91c8bc99bea61cb0e7d9
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-05-04 23:37:29 +1000

    Add title to landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="page">
-      <div className="title-2">
+      <div className="title-2 page-title-container h-[var(--title-font-size)]">
         <h2>PROJECTS</h2>
       </div>
     </div>
