2a8979af6d5c435367aebb8a342c1ff410a11f62
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-05-12 17:38:03 +1000

    Update ReHarvest with user journeys and images

--- src/app/design/ReHarvest/page.tsx
+++ src/app/design/ReHarvest/page.tsx
@@ -25,13 +25,39 @@ export default function ReHarvest() {
         <span className={`${styles.title}`}>
           <h2>ReHarvest</h2>
         </span>{" "}
-        <span className={`${styles.subtitle} ${styles.forestGreen}`}>
+        <span className={`${styles.subtitle} ${styles.text}`}>
           Rethinking Food Waste for Good
         </span>
       </div>
 
       {/* Body */}
       <div className={`${styles.bodyContainer}`}>
+        {/* Project Overview */}
+        <div className={`${styles.paraContainer}`}>
+          <div
+            className={`${styles.heading} ${styles.headingContainer} ${styles.forestGreen}`}
+          >
+            Project Overview
+          </div>
+          <div className={`${styles.bodyb} ${styles.forestGreen}`}>Team</div>
+          <div className={`${styles.body} grey`}>
+            I collaborated in a cross-functional hackathon team of five with two
+            front-end developers, two back-end developers, and myself as the
+            designer. I led the creative direction, defining the brand identity
+            and designing the user interface to ensure a visually engaging and
+            intuitive web experience.
+          </div>
+
+          <div className={`${styles.bodyb} ${styles.forestGreen}`}>Role</div>
+          <div className={`${styles.body} grey`}>
+            As the designer on the team, I led the end-to-end design process —
+            from initial ideation and user flows to creating low- and
+            high-fidelity prototypes. I developed a cohesive brand identity and
+            designed reusable UI components to ensure consistency and usability
+            across the platform.
+          </div>
+        </div>
+
         {/* Problem Statement */}
         <div className={styles.paraContainer}>
           <div
@@ -200,79 +226,103 @@ export default function ReHarvest() {
               </div>
 
               {/* Goals */}
-              <div className={`${styles.heading2} ${styles.grassGreen}`}>
-                Goals
-              </div>
-              <div className={`${styles.body} grey`}>
-                <ul className={`${styles.listContainer1}`}>
-                  <li>
-                    Find fresh produce that is affordable and within her budget
-                  </li>
-                  <li>
-                    Support local Australian farmers and sustainable food
-                    practices
-                  </li>
-                </ul>
+              <div className={styles.paraContainer1}>
+                {" "}
+                <div
+                  className={`${styles.heading2} ${styles.forestGreen} ${styles.iconText}`}
+                >
+                  <Target size={24} color="#82a36b" weight="fill" />
+                  <div className={`${styles.h2Padding}`}>Goals</div>
+                </div>
+                <div className={`${styles.body} grey`}>
+                  <ul className={`${styles.listContainer1}`}>
+                    <li>
+                      Find fresh produce that is affordable and within her
+                      budget
+                    </li>
+                    <li>
+                      Support local Australian farmers and sustainable food
+                      practices
+                    </li>
+                  </ul>
+                </div>
               </div>
 
               {/* Behavioural */}
-              <div className={`${styles.heading2} ${styles.grassGreen}`}>
-                Behavioural
-              </div>
-              <div className={`${styles.body} grey`}>
-                <ul className={`${styles.listContainer1}`}>
-                  <li>Shops for herself and her housemates</li>
-                  <li>
-                    Uses apps to compare grocery prices and track spending
-                  </li>
-                  <li>Interested in food rescue and low-waste living</li>
-                  <li>
-                    Prefers convenience but is happy to pick up produce locally
-                    if it saves money
-                  </li>
-                </ul>
+              <div className={styles.paraContainer1}>
+                <div
+                  className={`${styles.heading2} ${styles.forestGreen} ${styles.iconText}`}
+                >
+                  <ShoppingCartSimple size={24} color="#82a36b" weight="fill" />
+                  <div className={`${styles.h2Padding}`}>Behavioural</div>
+                </div>
+                <div className={`${styles.body} grey`}>
+                  <ul className={`${styles.listContainer1}`}>
+                    <li>Shops for herself and her housemates</li>
+                    <li>
+                      Uses apps to compare grocery prices and track spending
+                    </li>
+                    <li>Interested in food rescue and low-waste living</li>
+                    <li>
+                      Prefers convenience but is happy to pick up produce
+                      locally if it saves money
+                    </li>
+                  </ul>
+                </div>
               </div>
 
               {/* Pain Points */}
-              <div className={`${styles.heading2} ${styles.grassGreen}`}>
-                Pain Points{" "}
-              </div>
-              <div className={`${styles.body} grey`}>
-                <ul className={`${styles.listContainer1}`}>
-                  <li>
-                    Supermarket produce is often expensive and lacks
-                    transparency as to which farms it supports!
-                  </li>
-                  <li>
-                    Doesn&#39;t know what&#39;s in season or where to buy local
-                    produce affordably
-                  </li>
-                  <li>
-                    Wants to shop more ethically, but it&#39;s often
-                    inconvenient
-                  </li>
-                </ul>
+              <div className={styles.paraContainer1}>
+                {" "}
+                <div
+                  className={`${styles.heading2} ${styles.forestGreen} ${styles.iconText}`}
+                >
+                  <WarningCircle size={24} color="#82a36b" weight="fill" />
+                  <div className={`${styles.h2Padding}`}>Pain Points</div>
+                </div>
+                <div className={`${styles.body} grey`}>
+                  <ul className={`${styles.listContainer1}`}>
+                    <li>
+                      Supermarket produce is often expensive and lacks
+                      transparency as to which farms it supports!
+                    </li>
+                    <li>
+                      Doesn&#39;t know what&#39;s in season or where to buy
+                      local produce affordably
+                    </li>
+                    <li>
+                      Wants to shop more ethically, but it&#39;s often
+                      inconvenient
+                    </li>
+                  </ul>
+                </div>
               </div>
 
               {/* Needs */}
-              <div className={`${styles.heading2} ${styles.grassGreen}`}>
-                Needs
-              </div>
-              <div className={`${styles.body} grey`}>
-                <ul className={`${styles.listContainer1}`}>
-                  <li>
-                    A trustworthy, easy-to-use platform to browse and purchase
-                    discounted “rescued” produce
-                  </li>
-                  <li>
-                    Trust that produce is safe and good quality - even if it
-                    looks imperfect
-                  </li>
-                  <li>
-                    A sense that her purchase is making a positive social and
-                    environmental impact
-                  </li>
-                </ul>
+              <div className={styles.paraContainer1}>
+                {" "}
+                <div
+                  className={`${styles.heading2} ${styles.forestGreen} ${styles.iconText}`}
+                >
+                  <CheckCircle size={24} color="#82a36b" weight="fill" />
+                  <div className={`${styles.h2Padding}`}>Needs</div>
+                </div>
+                <div className={`${styles.body} grey`}>
+                  <ul className={`${styles.listContainer1}`}>
+                    <li>
+                      A trustworthy, easy-to-use platform to browse and purchase
+                      discounted “rescued” produce
+                    </li>
+                    <li>
+                      Trust that produce is safe and good quality - even if it
+                      looks imperfect
+                    </li>
+                    <li>
+                      A sense that her purchase is making a positive social and
+                      environmental impact
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
             <div className={`${styles.person}`}>
@@ -325,73 +375,96 @@ export default function ReHarvest() {
               </div>
 
               {/* Goals */}
-              <div className={`${styles.heading2} ${styles.grassGreen}`}>
-                Goals
-              </div>
-              <div className={`${styles.body} grey`}>
-                <ul className={`${styles.listContainer1}`}>
-                  <li>
-                    Reduce food waste and recover income from surplus/rejected
-                    produce
-                  </li>
-                  <li>Reach buyers beyond traditional distributors</li>
-                  <li>Build stronger local-buyer relationships</li>
-                </ul>
+              <div className={styles.paraContainer1}>
+                {" "}
+                <div
+                  className={`${styles.heading2} ${styles.forestGreen} ${styles.iconText}`}
+                >
+                  <Target size={24} color="#82a36b" weight="fill" />
+                  <div className={`${styles.h2Padding}`}>Goals</div>
+                </div>
+                <div className={`${styles.body} grey`}>
+                  <ul className={`${styles.listContainer1}`}>
+                    <li>
+                      Reduce food waste and recover income from surplus/rejected
+                      produce
+                    </li>
+                    <li>Reach buyers beyond traditional distributors</li>
+                    <li>Build stronger local-buyer relationships</li>
+                  </ul>
+                </div>
               </div>
 
               {/* Behavioural */}
-              <div className={`${styles.heading2} ${styles.grassGreen}`}>
-                Behavioural
-              </div>
-              <div className={`${styles.body} grey`}>
-                <ul className={`${styles.listContainer1}`}>
-                  <li>
-                    Regularly supplies large supermarkets, but deals with high
-                    cosmetic rejection rates
-                  </li>
-                  <li>
-                    Usually bins or compost tonnes of good produce each season
-                  </li>
-                </ul>
+              <div className={styles.paraContainer1}>
+                <div
+                  className={`${styles.heading2} ${styles.forestGreen} ${styles.iconText}`}
+                >
+                  <ShoppingCartSimple size={24} color="#82a36b" weight="fill" />
+                  <div className={`${styles.h2Padding}`}>Behavioural</div>
+                </div>
+                <div className={`${styles.body} grey`}>
+                  <ul className={`${styles.listContainer1}`}>
+                    <li>
+                      Regularly supplies large supermarkets, but deals with high
+                      cosmetic rejection rates
+                    </li>
+                    <li>
+                      Usually bins or compost tonnes of good produce each season
+                    </li>
+                  </ul>
+                </div>
               </div>
 
               {/* Pain Points */}
-              <div className={`${styles.heading2} ${styles.grassGreen}`}>
-                Pain Points{" "}
-              </div>
-              <div className={`${styles.body} grey`}>
-                <ul className={`${styles.listContainer1}`}>
-                  <li>
-                    Middlemen take significant margins, leaving low profit per
-                    kilo of sold produce
-                  </li>
-                  <li>
-                    Rejected produce creates disposal costs and environmental
-                    guilt
-                  </li>
-                  <li>No direct-to-buyer channel for “imperfect” produce</li>
-                </ul>
+              <div className={styles.paraContainer1}>
+                {" "}
+                <div
+                  className={`${styles.heading2} ${styles.forestGreen} ${styles.iconText}`}
+                >
+                  <WarningCircle size={24} color="#82a36b" weight="fill" />
+                  <div className={`${styles.h2Padding}`}>Pain Points</div>
+                </div>
+                <div className={`${styles.body} grey`}>
+                  <ul className={`${styles.listContainer1}`}>
+                    <li>
+                      Middlemen take significant margins, leaving low profit per
+                      kilo of sold produce
+                    </li>
+                    <li>
+                      Rejected produce creates disposal costs and environmental
+                      guilt
+                    </li>
+                    <li>No direct-to-buyer channel for “imperfect” produce</li>
+                  </ul>
+                </div>
               </div>
 
               {/* Needs */}
-              <div className={`${styles.heading2} ${styles.grassGreen}`}>
-                Needs
-              </div>
-              <div className={`${styles.body} grey`}>
-                <ul className={`${styles.listContainer1}`}>
-                  <li>
-                    A platform to quickly list available stock with basic
-                    details and pricing
-                  </li>
-                  <li>
-                    Visibility to nearby buyers (cafés, community orgs,
-                    households)
-                  </li>
-                  <li>
-                    Freedom to coordinate logistics independently (delivery,
-                    pickup, etc.)
-                  </li>
-                </ul>
+              <div className={styles.paraContainer1}>
+                {" "}
+                <div
+                  className={`${styles.heading2} ${styles.forestGreen} ${styles.iconText}`}
+                >
+                  <CheckCircle size={24} color="#82a36b" weight="fill" />
+                  <div className={`${styles.h2Padding}`}>Needs</div>
+                </div>
+                <div className={`${styles.body} grey`}>
+                  <ul className={`${styles.listContainer1}`}>
+                    <li>
+                      A platform to quickly list available stock with basic
+                      details and pricing
+                    </li>
+                    <li>
+                      Visibility to nearby buyers (cafés, community orgs,
+                      households)
+                    </li>
+                    <li>
+                      Freedom to coordinate logistics independently (delivery,
+                      pickup, etc.)
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
@@ -399,7 +472,44 @@ export default function ReHarvest() {
 
         {/* User Journeys */}
         <div className={`${styles.paraContainer}`}>
-          <div></div>
+          <div
+            className={`${styles.heading} ${styles.headingContainer} ${styles.forestGreen}`}
+          >
+            User Journeys
+          </div>
+
+          <div className={`flex jsutify-between`}>
+            <div
+              className={`flex flex-col items-center ${styles.heading2} ${styles.forestGreen} gap-3`}
+            >
+              <div>Customer</div>
+              <div>
+                <Image
+                  src={`${basePath}/images/JessUJ.png`}
+                  alt="Customer User Journey"
+                  width={574}
+                  height={1098}
+                  className={styles.image1}
+                />
+              </div>
+            </div>
+
+            <div
+              className={`flex flex-col items-center ${styles.heading2} ${styles.forestGreen} gap-3`}
+            >
+              <div>Wholesaler</div>
+              <div>
+                {" "}
+                <Image
+                  src={`${basePath}/images/PaulUJ.png`}
+                  alt="Wholesaler User Journey"
+                  width={572}
+                  height={1932}
+                  className={styles.image1}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
