2f6e7a084673e30bbfba248773bc30564f91567e
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-05-12 11:14:07 +1000

    Add project statement and competitor analysis

--- src/app/design/ReHarvest/page.tsx
+++ src/app/design/ReHarvest/page.tsx
@@ -3,33 +3,131 @@ import styles from "@/app/styles/ReHarvest.module.css";
 export default function ReHarvest() {
   return (
     <div className="page">
+      <div className={styles.container}></div>
       {/* Title */}
       <div className={`${styles.titleContainer}`}>
         <span className={`${styles.title}`}>
           <h2>ReHarvest:</h2>
         </span>{" "}
-        <span className={`${styles.subtitle} grey`}>
+        <span className={`${styles.subtitle} ${styles.forestGreen}`}>
           Rethinking Food Waste for Good
         </span>
       </div>
 
       {/* Body */}
       <div className={`${styles.bodyContainer}`}>
+        {/* Problem Statement */}
         <div className={styles.paraContainer}>
-          <div className={`${styles.heading} ${styles.headingContainer} olive`}>
-            [ Problem Statement ]
+          <div
+            className={`${styles.heading} ${styles.headingContainer} ${styles.forestGreen}`}
+          >
+            Problem Statement
           </div>
 
-          <div className={styles.body}>
-            Farmers across Australia are{" "}
-            <span className={styles.bodyb}>forced</span> to discard perfectly
-            good produce that doesn’t meet the strict cosmetic standards of
-            major supermarkets, leading to wasted resources like labour, water,
-            and fuel, and stripping farmers of potential income. Meanwhile,
-            consumers face inflated prices and rising food insecurity, missing
-            out on nutritious, affordable options that never reach shelves.
+          <div className={`${styles.body} grey`}>
+            Farmers across Australia are forced to discard{" "}
+            <span className={styles.bodyb}>perfectly good produce</span> that
+            doesn’t meet the{" "}
+            <span className={styles.bodyb}>strict cosmetic standards</span> of
+            major supermarkets, leading to{" "}
+            <span className={styles.bodyb}>wasted resources</span> like labour,
+            water, and fuel, and stripping farmers of potential income.
+            Meanwhile, consumers face{" "}
+            <span className={styles.bodyb}>inflated prices</span> and rising
+            food insecurity, missing out on nutritious, affordable options that
+            never reach shelves.
           </div>
         </div>
+
+        {/* Competitor Analysis */}
+
+        <div className={styles.paraContainer}>
+          <div
+            className={`${styles.heading} ${styles.headingContainer} ${styles.forestGreen}`}
+          >
+            Competitor Analysis
+          </div>
+
+          <div className={`${styles.body} grey`}>
+            Most food waste solutions focus on{" "}
+            <span className={styles.bodyb}>end-of-line fixes</span> like
+            upcycling or surplus redistribution, often missing the{" "}
+            <span className={styles.bodyb}>root causes</span>. ReHarvest takes a{" "}
+            <span className={styles.bodyb}>holistic </span>
+            approach, connecting{" "}
+            <span className={styles.bodyb}>wholesalers to consumers</span> and
+            rescuing <span className={styles.bodyb}>rejected produce</span> that
+            would otherwise go to waste.
+          </div>
+
+          <div className={`${styles.body} grey`}>
+            {" "}
+            Here's how current solutions fall short:
+          </div>
+
+          <ul className={`p-3 ${styles.body} ${styles.listContainer} grey`}>
+            <li>
+              Surplus food apps (e.g.{" "}
+              <a
+                href="https://toogoodtogo.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`${styles.link} ${styles.bodyi} ${styles.forestGreen}`}
+              >
+                Too Good To Go
+              </a>
+              ) <span className={styles.bodyb}>ignore upstream waste</span>.
+            </li>
+            <li>
+              Ugly produce boxes (e.g.{" "}
+              <a
+                href="https://www.farmerspick.com.au/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`${styles.link} ${styles.bodyi} ${styles.forestGreen}`}
+              >
+                Farmer’s Pick
+              </a>
+              ) have <span className={styles.bodyb}>limited options</span> and
+              <span className={styles.bodyb}> subscriptions</span>.
+            </li>
+            <li>
+              <span className={styles.bodyb}>Limited customization</span> and no
+              <span className={styles.bodyb}> large-scale ordering</span>.
+            </li>
+            <li>
+              Supermarkets like{" "}
+              <a
+                href="https://www.woolworths.com.au/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`${styles.link} ${styles.bodyi} ${styles.forestGreen}`}
+              >
+                Woolworths
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://www.coles.com.au/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`${styles.link} ${styles.bodyi} ${styles.forestGreen}`}
+              >
+                Coles
+              </a>{" "}
+              focus on <span className={styles.bodyb}>appearance</span>,
+              contributing to
+              <span className={styles.bodyb}> waste</span>.
+            </li>
+            <li>
+              Lack of <span className={styles.bodyb}>transparency</span> and
+              <span className={styles.bodyb}> flexibility</span> in
+              sustainability claims.
+            </li>
+          </ul>
+        </div>
+
+        {/* User Personas */}
+        <div className={styles.paraContainer}></div>
       </div>
     </div>
   );
