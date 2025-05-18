5d4540edf1d7497c865287c68754d6f4bf36856e
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-05-18 21:31:22 +1000

    Update contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+import styles from "@/app/styles/Contact.module.css";
+
 export default function Contact() {
   return (
     <div className="page">
@@ -6,6 +8,22 @@ export default function Contact() {
         style={{ color: "var(--slate-grey)" }}
       >
         <h2>Let&#39;s Connect</h2>
+      </div>
+
+      <div
+        className={`${styles.aboutContainer} ${styles.pixelBody} grey text-center`}
+      >
+        <div className={`${styles.small}`}>
+          Nguyen MT A. (aka AYIMA) is an aspiring{" "}
+        </div>
+        <div className={`${styles.pixelBodyB} ${styles.medium}`}>
+          CREATIVE, DESIGNER,
+        </div>
+        <div className={`${styles.small} ${styles.pixelBodyB}`}>&</div>
+        <div className={`${styles.pixelBodyB} ${styles.medium}`}>
+          SOFTWARE CRAFTSMAN
+        </div>{" "}
+        <div className={`${styles.small}`}>based in Melbourne, Australia.</div>
       </div>
     </div>
   );
