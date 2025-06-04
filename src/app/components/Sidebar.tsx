e8d4a35f8376b92a58eeae5337ced85a518abd28
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-06-04 17:34:36 +1000

    Try to fix sidebar transition

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -7,8 +7,57 @@ import config from "../../../next.config.js";
 import { usePathname } from "next/navigation";
 import { navLinks } from "../data/links";
 import { useEffect, useState } from "react";
+import type { NavLinkProps } from "../data/links";
 
 const basePath = config.basePath;
+
+interface SidebarLink {
+  href: string;
+  label: string;
+  color: string;
+}
+
+const SidebarLinkSection: React.FC<NavLinkProps> = ({
+  links,
+  direction,
+  pathname,
+}) => (
+  <div
+    className={
+      direction === "diagonal" ? styles.contactLinks : styles.pageLinks
+    }
+  >
+    {links.map((link: SidebarLink) => (
+      <NavLink key={link.href} direction={direction} alt="arrow">
+        <h3>
+          <Link
+            href={link.href}
+            className="link"
+            style={{
+              textShadow:
+                pathname === link.href
+                  ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
+                  : "var(--slate-grey)",
+              color: "var(--slate-grey)",
+              transition: "text-shadow 0.3s ease",
+            }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.textShadow = `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`)
+            }
+            onMouseLeave={(e) =>
+              (e.currentTarget.style.textShadow =
+                pathname === link.href
+                  ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
+                  : "none")
+            }
+          >
+            {link.label}
+          </Link>
+        </h3>
+      </NavLink>
+    ))}
+  </div>
+);
 
 const Sidebar: React.FC = () => {
   const pathname = usePathname();
@@ -16,26 +65,23 @@ const Sidebar: React.FC = () => {
   const [isProjectPage, setIsProjectPage] = useState(false);
 
   useEffect(() => {
-    // Only set the click handler on the client
     setHandleClick(() => () => {
       window.location.href = `${basePath}${navLinks.contact[0].href}`;
     });
-
-    const currentPath = window.location.pathname;
-    const segments = currentPath.split("/").filter(Boolean);
+    const segments = window.location.pathname.split("/").filter(Boolean);
     setIsProjectPage(segments.length >= 2);
   }, []);
 
   return (
     <div
+      className={`${styles.sidebarWrapper}`}
       style={{
         borderBottom: "var(--slate-grey) 1px solid",
         borderLeft: "var(--slate-grey) 1px solid",
       }}
     >
-      {isProjectPage && (
-        <div className={`${styles.sidebarCollapsed}`}>
-          {/* Name */}
+      {isProjectPage ? (
+        <div className={styles.sidebarCollapsed}>
           <div className={`title-11 grey flex ${styles.verticalNameContainer}`}>
             <Title
               className="title-11 flex"
@@ -47,96 +93,35 @@ const Sidebar: React.FC = () => {
               </Link>
             </Title>
           </div>
-
-          {/* Navigation Links */}
-          <div className={`${styles.navContainer}`}>
-            {/* Page Links */}
-            <div className={`${styles.pageLinks}`}>
-              {navLinks.main.map((link) => (
-                <NavLink key={link.href} direction="default" alt="arrow">
-                  <h3>
-                    <Link
-                      href={link.href}
-                      className={`link`}
-                      style={{
-                        textShadow:
-                          pathname === link.href
-                            ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
-                            : "var(--slate-grey)",
-                        color: `var(--slate-grey)`,
-                        transition: "text-shadow 0.3s ease", // Add transition for smooth animation
-                      }}
-                      onMouseEnter={(e) =>
-                        (e.currentTarget.style.textShadow = `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`)
-                      }
-                      onMouseLeave={(e) =>
-                        (e.currentTarget.style.textShadow =
-                          pathname === link.href
-                            ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
-                            : "none")
-                      }
-                    >
-                      {link.label}
-                    </Link>
-                  </h3>
-                </NavLink>
-              ))}
-            </div>
-
-            {/* Contact Links */}
-            <div className={`${styles.contactLinks}`}>
-              {navLinks.contact.map((link) => (
-                <NavLink key={link.href} direction="diagonal" alt="arrow">
-                  <h3>
-                    <Link
-                      href={link.href}
-                      className={`link`}
-                      style={{
-                        textShadow:
-                          pathname === link.href
-                            ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
-                            : "var(--slate-grey)",
-                        color: `var(--slate-grey)`,
-                        transition: "text-shadow 0.3s ease", // Add transition for smooth animation
-                      }}
-                      onMouseEnter={(e) =>
-                        (e.currentTarget.style.textShadow = `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`)
-                      }
-                      onMouseLeave={(e) =>
-                        (e.currentTarget.style.textShadow =
-                          pathname === link.href
-                            ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
-                            : "none")
-                      }
-                    >
-                      {link.label}
-                    </Link>
-                  </h3>
-                </NavLink>
-              ))}
-            </div>
+          <div className={styles.navContainer}>
+            <SidebarLinkSection
+              links={navLinks.main}
+              direction="default"
+              pathname={pathname}
+            />
+            <SidebarLinkSection
+              links={navLinks.contact}
+              direction="diagonal"
+              pathname={pathname}
+            />
           </div>
         </div>
-      )}
-      {!isProjectPage && (
+      ) : (
         <div className={styles.sidebarExpanded}>
-          {/* Title container */}
           <div className={styles.titleContainer}>
-            {/* Scrolling animated text */}
             <div
-              className={`scrolling-container`}
+              className="scrolling-container"
               onClick={handleClick ?? undefined}
               style={{ cursor: "pointer" }}
             >
               <span
-                className={`scrolling-text body grey`}
+                className="scrolling-text body grey"
                 style={{ cursor: "pointer" }}
               >
                 NGUYEN MT A. (AKA <span className="body-b">AYIMA</span>) IS AN
                 ASPIRING{" "}
                 <span className="body-bi">
-                  CREAT
-                  <span className="body-b">IV</span>E
+                  CREAT<span className="body-b">IV</span>E
                 </span>
                 ,{" "}
                 <span className="body-b">
@@ -149,8 +134,7 @@ const Sidebar: React.FC = () => {
                 BASED in MELBOURNE, AUSTRALIA. NGUYEN MT A. (AKA{" "}
                 <span className="body-b">AYIMA</span>) IS AN ASPIRING{" "}
                 <span className="body-bi">
-                  CREAT
-                  <span className="body-b">IV</span>E
+                  CREAT<span className="body-b">IV</span>E
                 </span>
                 ,{" "}
                 <span className="body-b">
@@ -160,11 +144,9 @@ const Sidebar: React.FC = () => {
                 <span className="body-b">
                   SOFTWARE <span className="body-bi">CRAFT</span>SMAN
                 </span>{" "}
-                BASED in MELBOURNE, AUSTRALIA.{" "}
+                BASED in MELBOURNE, AUSTRALIA.
               </span>
             </div>
-
-            {/* Title text */}
             <div className={`title-1 grey flex ${styles.nameContainer}`}>
               <Title className="title-1 flex" color="var(--marigold)">
                 <Link href="/">
@@ -172,8 +154,6 @@ const Sidebar: React.FC = () => {
                 </Link>
               </Title>
             </div>
-
-            {/* Image */}
             <div
               className={styles.imageContainer}
               style={{ overflow: "hidden" }}
@@ -188,12 +168,9 @@ const Sidebar: React.FC = () => {
               />
             </div>
           </div>
-
           <div className={styles.subContainer}>
-            {/* About Text */}
-            <div className={`${styles.aboutContainer}`}>
-              {/* Motion graphic */}
-              <div className={`${styles.imageContainer}`}>
+            <div className={styles.aboutContainer}>
+              <div className={styles.imageContainer}>
                 <Image
                   src={`${basePath}/images/lovely.png`}
                   alt="Motion graphic"
@@ -203,85 +180,26 @@ const Sidebar: React.FC = () => {
                   width={200}
                 />
               </div>
-
-              {/* About me text */}
-              <div className={`body-small text-justify grey`}>
-                I&#39;ve been orbiting around design my entire life - realised
+              <div className="body-small text-justify grey">
+                I&apos;ve been orbiting around design my entire life - realised
                 it as an afterthought and not a possibility. But time is much
-                too a fickle thing for me to mull and ponder over if only&#39;s.
-                Imagination should be brought to life. Welcome to my collection
-                of all things - lost and little - that I&#39;ve curated across
-                my years.
+                too a fickle thing for me to mull and ponder over if
+                only&apos;s. Imagination should be brought to life. Welcome to
+                my collection of all things - lost and little - that I&apos;ve
+                curated across my years.
               </div>
             </div>
-
-            {/* Navigation Links */}
-            <div className={`${styles.navContainer}`}>
-              {/* Page Links */}
-              <div className={`${styles.pageLinks}`}>
-                {navLinks.main.map((link) => (
-                  <NavLink key={link.href} direction="default" alt="arrow">
-                    <h3>
-                      <Link
-                        href={link.href}
-                        className={`link`}
-                        style={{
-                          textShadow:
-                            pathname === link.href
-                              ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
-                              : "var(--slate-grey)",
-                          color: `var(--slate-grey)`,
-                          transition: "text-shadow 0.3s ease", // Add transition for smooth animation
-                        }}
-                        onMouseEnter={(e) =>
-                          (e.currentTarget.style.textShadow = `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`)
-                        }
-                        onMouseLeave={(e) =>
-                          (e.currentTarget.style.textShadow =
-                            pathname === link.href
-                              ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
-                              : "none")
-                        }
-                      >
-                        {link.label}
-                      </Link>
-                    </h3>
-                  </NavLink>
-                ))}
-              </div>
-
-              {/* Contact Links */}
-              <div className={`${styles.contactLinks}`}>
-                {navLinks.contact.map((link) => (
-                  <NavLink key={link.href} direction="diagonal" alt="arrow">
-                    <h3>
-                      <Link
-                        href={link.href}
-                        className={`link`}
-                        style={{
-                          textShadow:
-                            pathname === link.href
-                              ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
-                              : "var(--slate-grey)",
-                          color: `var(--slate-grey)`,
-                          transition: "text-shadow 0.3s ease", // Add transition for smooth animation
-                        }}
-                        onMouseEnter={(e) =>
-                          (e.currentTarget.style.textShadow = `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`)
-                        }
-                        onMouseLeave={(e) =>
-                          (e.currentTarget.style.textShadow =
-                            pathname === link.href
-                              ? `calc(var(--square-size) * 0.02) 0px 0px ${link.color}`
-                              : "none")
-                        }
-                      >
-                        {link.label}
-                      </Link>
-                    </h3>
-                  </NavLink>
-                ))}
-              </div>
+            <div className={styles.navContainer}>
+              <SidebarLinkSection
+                links={navLinks.main}
+                direction="default"
+                pathname={pathname}
+              />
+              <SidebarLinkSection
+                links={navLinks.contact}
+                direction="diagonal"
+                pathname={pathname}
+              />
             </div>
           </div>
         </div>
